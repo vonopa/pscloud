b2b0ea884b09341ac2dcc527aefbd22311619457
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2019-05-08 12:17:48 +0300

    Catch internal server error from remote on decline

--- apps/federatedfilesharing/lib/AppInfo/Application.php
+++ apps/federatedfilesharing/lib/AppInfo/Application.php
@@ -34,6 +34,7 @@ use OCA\FederatedFileSharing\Ocm\NotificationManager;
 use OCA\FederatedFileSharing\Ocm\Permissions;
 use OCA\FederatedFileSharing\TokenHandler;
 use OCP\AppFramework\App;
+use OCP\AppFramework\Http;
 use OCP\Share\Events\AcceptShare;
 use OCP\Share\Events\DeclineShare;
 
@@ -259,11 +260,19 @@ class Application extends App {
 			function (DeclineShare $event) use ($container) {
 				/** @var Notifications $notifications */
 				$notifications = $container->query('Notifications');
-				$notifications->sendDeclineShare(
-					$event->getRemote(),
-					$event->getRemoteId(),
-					$event->getShareToken()
-				);
+				try {
+					$notifications->sendDeclineShare(
+						$event->getRemote(),
+						$event->getRemoteId(),
+						$event->getShareToken()
+					);
+				} catch (\Exception $e) {
+					// ownCloud lower than 10.2 responded with Internal Server Error
+					// on declining non-existing share. It can't be caught outside the closure
+					if ($e->getCode() !== Http::STATUS_INTERNAL_SERVER_ERROR) {
+						throw $e;
+					}
+				}
 			}
 		);
 	}
